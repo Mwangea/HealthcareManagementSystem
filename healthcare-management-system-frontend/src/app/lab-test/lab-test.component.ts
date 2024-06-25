3a7247520c94209567ea96b5b2f0ec7c444b033a
mwangea <mwangeamusa@gmail.com>
2024-06-25 08:51:44 +0300

    adding dependancies in the frontend

---
+++ healthcare-management-system-frontend/src/app/lab-test/lab-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './lab-test.component.html',
+  styleUrl: './lab-test.component.scss'
+})
+export class LabTestComponent {
+
+}
